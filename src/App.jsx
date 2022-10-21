d7e9417a6af562d56e0334e93aa3a53db0dcfb87
danixy8 <danielguve2@gmail.com>
2022-10-21 12:43:39 -0300

    correccion de importacion Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import Header from './components/header'
+import Header from './components/Header'
 import BudgetList from './components/BudgetList'
 import Modal from './components/Modal'
 import { generateId } from './helpers'
